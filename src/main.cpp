6687f2f14df2012c89f374bf1cc53645f2b630a6
CollinQuiason <CollinRottinghaus@gmail.com>
2026-08-21 07:04:37 -0500

    same issue with esp32 library for encoder

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
+
+#include "ESP32Encoder.h"
 #include "ssd1306.h"
 
 /* ######## Config */
@@ -16,93 +18,14 @@ constexpr float DIVIDER_RATIO = 16.0f;
   // Encoder
 constexpr uint32_t DEBOUNCE_MS = 25;
 constexpr int8_t EDGES_PER_DETENT = 4;
-portMUX_TYPE encoderMux = portMUX_INITIALIZER_UNLOCKED;
 
-/* ######## Global Variables */
+
+/* ######## Globals */
   // Encoder
-volatile int32_t encoderEdgeDelta = 0;
-volatile uint8_t previousAB = 0;
-
-
-DRAM_ATTR static const int8_t QUADRATURE_TABLE[16] = { // Rejects invalid transitions caused by contact bounce.{
-    0, -1,  1,  0,
-    1,  0,  0, -1,
-   -1,  0,  0,  1,
-    0,  1, -1,  0
-};
-/* ######## */
-int interruptCount = 0;
-void IRAM_ATTR encoderISR() {
-    interruptCount++;
-    uint8_t currentAB =
-        (digitalRead(ENCODER_A) << 1) |
-         digitalRead(ENCODER_B);
-
-    uint8_t transition = (previousAB << 2) | currentAB;
-    previousAB = currentAB;
-
-    portENTER_CRITICAL_ISR(&encoderMux);
-    encoderEdgeDelta += QUADRATURE_TABLE[transition];
-    portEXIT_CRITICAL_ISR(&encoderMux);
-}
-
-int32_t readEncoderChange() {
-    portENTER_CRITICAL(&encoderMux);
-    int32_t newEdges = encoderEdgeDelta;
-    encoderEdgeDelta = 0;
-    portEXIT_CRITICAL(&encoderMux);
-
-    static int32_t remainder = 0;
-    int32_t detents = 0;
-
-    remainder += newEdges;
-
-    while (remainder >= EDGES_PER_DETENT) {
-        remainder -= EDGES_PER_DETENT;
-        detents++;
-    }
-
-    while (remainder <= -EDGES_PER_DETENT) {
-        remainder += EDGES_PER_DETENT;
-        detents--;
-    }
-
-    return detents;
-}
-
-bool buttonPressed() {
-
-    static bool previousRaw = HIGH;
-    static bool stableState = HIGH;
-    static uint32_t lastChangeTime = 0;
-
-    bool raw = digitalRead(ENCODER_BUTTON);
-
-    if (raw != previousRaw) {
-        previousRaw = raw;
-        lastChangeTime = millis();
-    }
-
-    if (millis() - lastChangeTime >= DEBOUNCE_MS &&
-        raw != stableState) {
-
-        stableState = raw;
-
-        if (stableState == LOW)
-            return true;
-        }
-
-    return false;
-}
+ESP32Encoder encoder;
 
 bool buttonHeld() {
     return !digitalRead(ENCODER_BUTTON);
-}
-
-void drawLine() {
-    char line [22];
-    snprintf(line, sizeof(line), "Hello World!", "");
-    ssd1306_printFixed(0, 0, line, STYLE_NORMAL);
 }
 
 
@@ -128,69 +51,57 @@ void setup() {
     printf("OLED panel initialized!\n");
       // Encoder
     printf("Initializing encoder...\n");
-    pinMode(ENCODER_A, INPUT_PULLUP);
-    pinMode(ENCODER_B, INPUT_PULLUP);
+    ESP32Encoder::useInternalWeakPullResistors = puType::up;
+    encoder.attachFullQuad(ENCODER_A, ENCODER_B);
+    encoder.setFilter(1023);
+    encoder.clearCount();
     pinMode(ENCODER_BUTTON, INPUT_PULLUP);
-    previousAB =
-    (digitalRead(ENCODER_A) << 1) |
-     digitalRead(ENCODER_B);
-    attachInterrupt(
-        digitalPinToInterrupt(ENCODER_A),
-        encoderISR,
-        CHANGE
-    );
-    attachInterrupt(
-        digitalPinToInterrupt(ENCODER_B),
-        encoderISR,
-        CHANGE
-    );
     printf("Encoder initialized!\n");
       // Done
     printf("Setup done!\n");
 }
 
-void loop() {
-    float rawMV1 = analogReadMilliVolts(VOLTAGE_PIN_1);
-    float rawMV2 = analogReadMilliVolts(VOLTAGE_PIN_2);
-    float adjustedV1 = rawMV1 * DIVIDER_RATIO / 1000.0f;
-    float adjustedV2 = rawMV2 * DIVIDER_RATIO / 1000.0f;
+// void loop() {
+//     float rawMV1 = analogReadMilliVolts(VOLTAGE_PIN_1);
+//     float rawMV2 = analogReadMilliVolts(VOLTAGE_PIN_2);
+//     float adjustedV1 = rawMV1 * DIVIDER_RATIO / 1000.0f;
+//     float adjustedV2 = rawMV2 * DIVIDER_RATIO / 1000.0f;
+//
+//     /* ######## Screen Updates */
+//       // Formatting
+//     char rawVsens1Charp[10];
+//     char rawVsens2Charp[10];
+//     char selectedValueCharp[10];
+//     snprintf(rawVsens1Charp, sizeof(rawVsens1Charp), "%.3f", adjustedV1);
+//     snprintf(rawVsens2Charp, sizeof(rawVsens2Charp), "%.3f", adjustedV2);
+//       // Output
+//     ssd1306_printFixed(0, 8,  rawVsens1Charp, STYLE_NORMAL);
+//     ssd1306_printFixed(0, 16,  rawVsens2Charp, STYLE_NORMAL);
+//
+//
+//
+//     if (buttonHeld()) {
+//         ssd1306_printFixed(0, 32, "Pressed!", STYLE_NORMAL);
+//     } else {
+//         ssd1306_printFixed(0, 32, "        ", STYLE_NORMAL);
+//     }
+//
+//     printf(reinterpret_cast<const char*>(encoder.getCount()));
+//
+//     char interruptCountCharp[10];
+//     snprintf(interruptCountCharp, sizeof(interruptCountCharp), "%d", encoder.getCount());
+//     ssd1306_printFixed(0, 40,  interruptCountCharp, STYLE_NORMAL);
+// }
 
-    static int32_t selectedValue = 0;
+void loop()
+{
+    encoder.tick();
 
-    int32_t movement = readEncoderChange();
+    static long previous = 0;
+    long position = encoder.getPosition();
 
-    if (movement != 0) {
-        selectedValue += movement;
-        Serial.printf("Value: %ld\n", selectedValue);
+    if (position != previous) {
+        previous = position;
+        // Update selected value.
     }
-
-    if (buttonPressed()) {
-        Serial.println("Encoder pressed");
-    }
-
-
-
-    /* ######## Screen Updates */
-      // Formatting
-    char rawVsens1Charp[10];
-    char rawVsens2Charp[10];
-    char selectedValueCharp[10];
-    snprintf(rawVsens1Charp, sizeof(rawVsens1Charp), "%.3f", adjustedV1);
-    snprintf(rawVsens2Charp, sizeof(rawVsens2Charp), "%.3f", adjustedV2);
-    snprintf(selectedValueCharp, sizeof(selectedValueCharp), "%d", selectedValue);
-      // Output
-    ssd1306_printFixed(0, 8,  rawVsens1Charp, STYLE_NORMAL);
-    ssd1306_printFixed(0, 16,  rawVsens2Charp, STYLE_NORMAL);
-    ssd1306_printFixed(0, 24,  selectedValueCharp, STYLE_NORMAL);
-    if (buttonHeld()) {
-        ssd1306_printFixed(0, 32, "Pressed!", STYLE_NORMAL);
-    } else {
-        ssd1306_printFixed(0, 32, "        ", STYLE_NORMAL);
-    }
-
-
-    char interruptCountCharp[10];
-    snprintf(interruptCountCharp, sizeof(interruptCountCharp), "%d", interruptCount);
-    ssd1306_printFixed(0, 40,  interruptCountCharp, STYLE_NORMAL);
 }
-
